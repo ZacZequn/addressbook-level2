be1e532fab5bfd57059f606766ebe34a7e0f4c3e
Zhang Zequn <zhangzequn96@gmail.com>
2018-02-15 18:19:03 +0800

    Update I/O test cases and fixed some minor bugs

--- src/seedu/addressbook/commands/EditCommand.java
+++ src/seedu/addressbook/commands/EditCommand.java
@@ -23,11 +23,11 @@ public class EditCommand extends Command{
 
 	public static final String COMMAND_WORD = "edit";
 
-	public static final String MESSAGE_USAGE = COMMAND_WORD  ": Edits a person in the address book. "
-		"Contact details can be re-marked private by prepending 'p' to the prefix or not.\n"
-		"Parameters: NAME [p]p/PHONE [p]e/EMAIL [p]a/ADDRESS  [t/TAG]...\n"
-		"Example: "  COMMAND_WORD
-		" John Doe p/98765432 e/johnd@gmail.com a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney";
+	public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits a person in the address book. "
+		+ "Contact details can be re-marked private by prepending 'p' to the prefix or not.\n"
+		+ "Parameters: NAME [p]p/PHONE [p]e/EMAIL [p]a/ADDRESS  [t/TAG]...\n"
+		+ "Example: " + COMMAND_WORD
+		+ " John Doe p/98765432 e/johnd@gmail.com a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney";
 
 	public static final String MESSAGE_SUCCESS = "Person edited: %1$s";
 	public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
